3d3f5345d394b477544cc3965cd8a043f224e0d1
Billzerma <nabilzerma272002@gmail.com>
2024-05-13 23:13:34 +0700

    sudah bisa git

--- app/src/main/java/com/sukasrana/peka/MainActivity.kt
+++ app/src/main/java/com/sukasrana/peka/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PekaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Nabil Mengedit bang",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
